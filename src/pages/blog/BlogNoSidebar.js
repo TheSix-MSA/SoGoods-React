133c878a631d1fa394311019f4fd74795745d71c
chnew12 <jaey1229@gmail.com>
2021-07-19 21:01:42 +0900

    board 작업중

--- src/pages/blog/BlogNoSidebar.js
+++ src/pages/blog/BlogNoSidebar.js
@@ -1,49 +1,68 @@
 import PropTypes from "prop-types";
-import React, { Fragment } from "react";
+import React, {Fragment, useEffect, useState} from "react";
 import MetaTags from "react-meta-tags";
-import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
 import LayoutOne from "../../layouts/LayoutOne";
 import Breadcrumb from "../../wrappers/breadcrumb/Breadcrumb";
 import BlogPagination from "../../wrappers/blog/BlogPagination";
 import BlogPostsNoSidebar from "../../wrappers/blog/BlogPostsNoSidebar";
+import boardService from "./boardService";
 
+const initState = {
+    boardDtoList: [],
+    boardListRequestDTO:{
+        page:1,
+        size:0,
+        keyword:'',
+        type:''
+    },
+    pageMaker:{
+        page:1,
+        size:0,
+        totalCount:0,
+        pageList:[],
+        prev:false,
+        next:false
+    }
+}
 const BlogNoSidebar = ({ location }) => {
-  const { pathname } = location;
+    const { pathname } = location;
+    const [data, setData] = useState(initState);
 
+    useEffect(() => {
+      boardService.getBoardList().then(res => {
+        setData(res)
+      })
+    }, [])
+    console.log(data)
   return (
     <Fragment>
-      <MetaTags>
-        <title>Flone | Blog</title>
-        <meta
-          name="description"
-          content="Blog of flone react minimalist eCommerce template."
-        />
-      </MetaTags>
-      <BreadcrumbsItem to={process.env.PUBLIC_URL + "/"}>Home</BreadcrumbsItem>
-      <BreadcrumbsItem to={process.env.PUBLIC_URL + pathname}>
-        Blog
-      </BreadcrumbsItem>
-      <LayoutOne headerTop="visible">
-        {/* breadcrumb */}
-        <Breadcrumb />
-        <div className="blog-area pt-100 pb-100 blog-no-sidebar">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12">
-                <div className="mr-20">
-                  <div className="row">
-                    {/* blog posts */}
-                    <BlogPostsNoSidebar />
-                  </div>
+              <MetaTags>
+                <meta
+                  name="description"
+                  content="Blog of flone react minimalist eCommerce template."
+                />
+              </MetaTags>
+              <LayoutOne headerTop="visible">
+                {/* breadcrumb */}
+                <Breadcrumb />
+                <div className="blog-area pt-100 pb-100 blog-no-sidebar">
+                  <div className="container">
+                    <div className="row">
+                      <div className="col-lg-12">
+                        <div className="mr-20">
+                          <div className="row">
+                            {/* blog posts */}
+                            <BlogPostsNoSidebar />
+                          </div>
 
-                  {/* blog pagination */}
-                  <BlogPagination />
+                          {/* blog pagination */}
+                          <BlogPagination />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </LayoutOne>
+              </LayoutOne>
     </Fragment>
   );
 };

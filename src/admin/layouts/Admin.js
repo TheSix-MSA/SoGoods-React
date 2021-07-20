ce395454c286ed513af9a6f6c07edbc042a48509
sukhyunil <sukhyunil@naver.com>
2021-07-20 18:34:25 +0900

    merge wit dashboard template

---
+++ src/admin/layouts/Admin.js
@@ -0,0 +1,78 @@
+import React, { Component, Fragment } from "react";
+import { useLocation, Route, Switch } from "react-router-dom";
+import TableList from "../views/TableList";
+import Sidebar from "../components/Sidebar/Sidebar";
+import AdminNavbar from "../components/Navbars/AdminNavbar";
+import FooterOne from "../../wrappers/footer/FooterOne";
+import PropTypes from "prop-types";
+import LayoutOne from "../../layouts/LayoutOne";
+
+// import AdminNavbar from "components/Navbars/AdminNavbar";
+// import Footer from "components/Footer/Footer";
+
+
+
+import routes from "./routes.js";
+
+// import sidebarImage from "assets/img/sidebar-3.jpg";
+
+function Admin(){
+
+  // const [image, setImage] = React.useState(sidebarImage);
+  // const [color, setColor] = React.useState("black");
+  // const [hasImage, setHasImage] = React.useState(true);
+  const location = useLocation();
+
+  const mainPanel = React.useRef(null);
+  const getRoutes = (routes) => {
+    return routes.map((prop, key) => {
+      if (prop.layout === "/admin") {
+        return (
+          <Route
+            path={prop.layout + prop.path}
+            render={(props) => <prop.component {...props} />}
+            key={key}
+          />
+        );
+      } else {
+        return null;
+      }
+    });
+  };
+
+  React.useEffect(() => {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    mainPanel.current.scrollTop = 0;
+    if (
+      window.innerWidth < 993 &&
+      document.documentElement.className.indexOf("nav-open") !== -1
+    ) {
+      document.documentElement.classList.toggle("nav-open");
+      const element = document.getElementById("bodyClick");
+      element.parentNode.removeChild(element);
+    }
+  }, [location]);
+  return (
+    <>
+      <div className="wrapper">
+        <Sidebar routes={routes} />
+        {/*<Sidebar/>*/}
+        <div className="main-panel" ref={mainPanel}>
+        {/*<div className="main-panel">*/}
+          <AdminNavbar />
+          <div className="content">
+            <Switch>{getRoutes(routes)}</Switch>
+          </div>
+          <FooterOne
+              backgroundColorClass="bg-gray"
+              spaceTopClass="pt-100"
+              spaceBottomClass="pb-70"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Admin;

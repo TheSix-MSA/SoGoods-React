c91e7b7f544dc33bd002565a35ab154ee62469a1
Hyunil Suk <74586346+sukhyunil@users.noreply.github.com>
2021-07-25 18:08:27 +0900

    image모달창 진행중

--- src/admin/views/NoticeTable.js
+++ src/admin/views/NoticeTable.js
@@ -48,7 +48,6 @@ const param = {
     keyword: ''
 }
 const NoticeTable = () => {
-    const classes = useStyles();
     const history = useHistory()
     const location = useLocation();
 
@@ -112,7 +111,6 @@ const NoticeTable = () => {
         })
     }
 
-
     const list = notices.boardDtoList.map(notice => {
         return <tr key={notice.bno}>
             <td onClick={() => toNotice(notice.bno)}>{notice.title}</td>
@@ -120,8 +118,8 @@ const NoticeTable = () => {
             <td>{notice.content}</td>
             <td>{getFormatDate(new Date(notice.regDate))}</td>
             <td>{getFormatDate(new Date(notice.modDate))}</td>
-            <td onClick={() => changeRemoved(notice)}>{notice.removed ? "⭕" : "❌"}</td>
-            <td onClick={() => changePrivate(notice)}>{notice.private ? "⭕" : "❌"}</td>
+            <td onClick={() => changeRemoved(notice)}>{notice.removed ? "삭제됨" : "정상"}</td>
+            <td onClick={() => changePrivate(notice)}>{notice.private ? "공지중" : "공지 X"}</td>
         </tr>
 
     })
@@ -133,41 +131,23 @@ const NoticeTable = () => {
                     <Card.Header>
                         <Card.Title as="h4">공지 리스트</Card.Title>
 
+                            <div className="pro-sidebar-search mb-55 mt-25">
+                                <form className="pro-sidebar-search-form" action="#">
+                                    <select name="type" style={{width:"10%"}} onChange={searchOnChange}>
+                                        <option value='t'>제목</option>
+                                        <option value='w'>작성자</option>
+                                        <option value='c'>내용</option>
+                                        <option value='tc'>제목+내용</option>
+                                    </select>
+                                    <input value={searchInput.keyword} onChange={searchOnChange} type="text"
+                                           name="keyword" placeholder="검색"/>
+                                    <button style={{top:"70%"}} onClick={search}>
+                                        <i className="pe-7s-search" />
+                                    </button>
+                                </form>
+                            </div>
+
                         <Register/>
-
-                        <div className="pro-sidebar-search mb-55 mt-25">
-                            <FormControl className={classes.formControl}>
-                                <InputLabel shrink id="demo-simple-select-placeholder-label-label">
-                                    선택
-                                </InputLabel>
-                                <Select labelId="demo-simple-select-placeholder-label-label"
-                                        id="demo-simple-select-placeholder-label"
-                                        displayEmpty
-                                        className={classes.selectEmpty}
-                                        name="type" onChange={searchOnChange}>
-                                    <MenuItem value="t"> 제목</MenuItem>
-                                    <MenuItem value="w"> 작성자</MenuItem>
-                                    <MenuItem value="c"> 내용</MenuItem>
-                                    <MenuItem value="tc"> 제목+내용</MenuItem>
-                                </Select>
-                            </FormControl>
-                            <TextField
-                                style={{width: "40%", margin: "8px"}}
-                                id="standard-basic"
-                                label="검색어"
-                                name="keyword"
-                                value={searchInput.keyword}
-                                onChange={searchOnChange}/>
-                            <Button variant="outlined" onClick={search}
-                                    style={{marginTop: "15px", padding: "15px 15px"}}>
-                                <i className="pe-7s-search"/>
-                            </Button>
-                        </div>
-
-                        <p className="card-category">
-                            공지 정보
-                        </p>
-
                     </Card.Header>
                     <Card.Body className="table-full-width table-responsive px-0">
                         <Table className="table-hover table-striped" style={{textAlign: "center"}}>
@@ -179,7 +159,7 @@ const NoticeTable = () => {
                                 <th className="border-0">생성 날짜</th>
                                 <th className="border-0">수정 날짜</th>
                                 <th className="border-0">삭제 여부</th>
-                                <th className="border-0">숨김</th>
+                                <th className="border-0">공지</th>
                             </tr>
                             </thead>
                             <tbody>
@@ -193,25 +173,4 @@ const NoticeTable = () => {
         </Row>
     );
 }
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        '& > *': {
-            margin: theme.spacing(1),
-        },
-    },
-    margin: {
-        margin: theme.spacing(1),
-    },
-    extendedIcon: {
-        marginRight: theme.spacing(1),
-    },
-    formControl: {
-        margin: theme.spacing(1),
-        minWidth: 120,
-    },
-    selectEmpty: {
-        marginTop: theme.spacing(2),
-    },
-}));
 export default NoticeTable;

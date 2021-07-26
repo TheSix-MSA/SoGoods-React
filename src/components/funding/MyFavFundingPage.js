8e9e679832a5eab65b1fdacc28ee3ee12046ad6d
ChanWoo Park <diqksk@naver.com>
2021-07-27 00:40:36 +0900

    My Favorite Goods

--- src/components/funding/MyFavFundingPage.js
+++ src/components/funding/MyFavFundingPage.js
@@ -2,8 +2,6 @@ import React, {Fragment, useEffect, useState} from 'react';
 import {useHistory} from "react-router-dom";
 import {useSelector} from "react-redux";
 import fundingService from "./fundingService";
-import MetaTags from "react-meta-tags";
-import LayoutOne from "../layouts/header/LayoutOne";
 import Nav from "react-bootstrap/Nav";
 
 const initList = [{
@@ -45,43 +43,36 @@ const MyFavFundingPage = ({productTabClass}) => {
         history.push("/funding/read/"+fno)
     }
 
-    const listPage = list.map((li, idx)=>
+    const listPage = list.map((li, idx) =>
         !list ?
             <div>
                 <h4>찜한 내역이 존재하지 않습니다.</h4>
             </div>
             :
-            <div key={idx}>
-                <h5>{li.fno}번 게시글</h5>
-                {!li.success ?
-                    <div>
-                    <img alt={"이미지"} src={li.imgSrc||process.env.PUBLIC_URL+"/assets/img/default.png"} height={"230px"} width={"350px"} onClick={() => readTodo(li.fno)} style={{cursor: "pointer", objectFit:"cover"}}/>
-                    </div>
+            <div key={idx} style={{display:"flex", justifyContent:"center", padding:"15px", border:"1px solid rgba(0, 0, 0, 0.125)"}}>
+                <div>
+                    {!li.success ?
+                        <div style={{marginBottom:"10px"}}>
+                            <img alt={"이미지"} src={li.imgSrc || process.env.PUBLIC_URL + "/assets/img/default.png"}
+                                  onClick={() => readTodo(li.fno)}
+                                 style={{cursor: "pointer",width:"150px", height:"150px" , objectFit: "cover"}}/>
+                        </div>
                         :
-                    <div style={{position:"relative", height:"230px", width:"350px"}}>
-                        <img alt={"이미지"} src={li.imgSrc||process.env.PUBLIC_URL+"/assets/img/default.png"} style={{filter:"grayscale(100%)", objectFit:"cover", height:"230px", width:"350px"}}/>
-                        <span style={{fontWeight:"bold", position:"absolute", transform:"translate(-50%, -50%)", top:"50%", left:"50%", pointerEvents:"none"}}>
-                            <h3 style={{fontWeight:"bold"}}>종료된 펀딩입니다</h3>
-                        </span>
-                    </div>
-                }
-                <h5>펀딩 달성률 {Math.ceil(li.totalAmount / li.targetAmount * 100)}%</h5>
-                <h5>마감일자 : {li.dueDate}</h5>
-                <h5>펀딩금액 : {li.totalAmount}</h5>
+                        <div style={{marginBottom:"10px" ,width:"150px", height:"150px", backgroundImage:`url(${li.imgSrc || process.env.PUBLIC_URL + "/assets/img/default.png"})`
+                            , display:"flex", justifyContent:"center", alignItems:"center", filter: "grayscale(100%)"}}>
+                            <p style={{fontWeight:"bold", color:"red"}}>완료된 펀딩입니다</p>
+                        </div>
+                    }
+                    <h5 style={{margin:0}}>펀딩 달성률 {Math.ceil(li.totalAmount / li.targetAmount * 100)}%</h5>
+                    <h5 style={{margin:0}}>마감일자 : {li.dueDate}</h5>
+                    <h5 style={{margin:0}}>펀딩금액 : {li.totalAmount}</h5>
+                </div>
             </div>
-    )
+    );
 
     return (
         <div>
             <Fragment>
-                <MetaTags>
-                    <title>SoGoods</title>
-                    <meta
-                        name="description"
-                        content="Furniture home of flone react minimalist eCommerce template."
-                    />
-                </MetaTags>
-                <LayoutOne headerTop="visible">
                     <Nav
                         variant="pills"
                         className={`product-tab-list-6 justify-content-center mb-60 ${
@@ -97,13 +88,12 @@ const MyFavFundingPage = ({productTabClass}) => {
                             <div className="col-lg-12">
                                 <div style={{float:"right"}}>
                                 </div>
-                                <div style={{display:"grid", gridTemplateColumns: "1fr 1fr 1fr" ,gridTemplateRows: "1fr 1fr 1fr"}}>
+                                <div style={{display:"grid", gridTemplateColumns: "1fr 1fr 1fr" ,gridTemplateRows: "1fr 1fr 1fr", justifyContent:"center", alignItems:"center"}}>
                                     {listPage}
                                 </div>
                             </div>
                         </div>
                     </div>
-                </LayoutOne>
             </Fragment>
 
         </div>
